855648b869043e57caa29ad646e5dfcdbf8abade
jkumaraguru <jkumaraguru.murthy@gmail.com>
2024-11-04 12:01:39 +0530

    Product Model And Migration
    php artisan make:model Product -m

---
+++ app/Models/Product.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use Illuminate\Support\Facades\Storage;
+
+class Product extends Model
+{
+   use HasFactory;
+
+   protected $fillable = [
+       'name',
+       'price',
+       'category_id',
+       'brand_id',
+       'qty',
+       'alert_stock',
+       'image_path',
+       'description',
+];
+
+public function category(): BelogsTo
+{
+    return $this->belongsTo(category::class);
+}
+
+
+public function brand(): BelogsTo
+{
+    return $this->belongsTo(brand::class);
+}
+
+
+public function GetImagePath()
+{
+    return env('DOMAIN_URL') . Storage::url($this->image_path);
+}
+
+}
+
