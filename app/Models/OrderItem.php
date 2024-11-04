6ba4a5d516059e751fa87b383d782f7878c9f270
jkumaraguru <jkumaraguru.murthy@gmail.com>
2024-11-04 13:49:56 +0530

    Order Item Migration Model
    php artisan make:OrderItem -m

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class OrderItem extends Model
+{
+    
+    use HasFactory;
+
+    protected $fillable = [
+        
+
+        'order_id',
+        'product_id',
+        'qty',
+        'unit_price',
+        'amount',
+        'discount'
+
+    ];
+    
+
+    public function order()
+    {
+        return $this-belongsTo(Order::class);
+    }
+
+
+    public function product()
+    {
+        return $this-belongsTo(Product::class);
+    }
+}
